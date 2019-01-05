e421021b9c173404d1a8efaa18ecc80a8a11177d
silvicardo <silvicardo@gmail.com>
2019-01-05 16:13:29 +0100

    snack 1 2 3

--- main.js
+++ main.js
@@ -1,17 +1,98 @@
 console.log('welcome 2019');
 
-/*
-API Snack 4
-Chiedere all’utente quanti elementi vuole includere nell’array.
-Generare attraverso l’API un array contenente N numeri compresi tra 1 e 100.
-Stampare a schermo la somma dei numeri generati.
-(www.boolean.careers/api/array/integers, attenzione, serve query string)
-*/
-
+// RIMUOVERE I COMMENTI DALLO SNACK CHE SI VUOLE TESTARE
 $(document).ready(function () {
 
-  var numeri = [];
-  //DA RIVEDER ENDPOINT
+  /*
+  API SNACK 1
+  Creare un blocco html che conterrà numeri dispari e un altro contenente che
+  conterrà i numeri pari. Ad ogni click su un bottone chiedere all’API un numero,
+  se è dispari metterlo nel blocco dispari, e se è pari in quello pari.
+  */
+
+  $('#btnSnack1').click(function () {
+
+    var numero = parseInt($('#inputNumerico').val());
+
+    $('#inputNumerico').val('');
+
+    var isEven = (numero % 2 === 0);
+
+    (isEven === true) ? $('.numeriPari').append(numero + ', ') : $('.numeriDispari').append(numero + ', ');
+
+  });
+
+  /*
+  API SNACK 2
+  Creare un array contenente la lista di 10 invitati ad una festa, chiedendo
+  all’API 10 nomi random per popolare l’array.
+  Una volta generata questa lista chiedere all’utente di dire il proprio nome. Se è
+  uno degli invitati ritornare un messaggio di benvenuto, altrimenti di accesso
+  negato.
+  */
+
+  /*
+  var invitati = [];
+  console.log('Hey, accepted guests to the party are: ');
+  for (var i = 0; i < 10; i++) {
+    $.ajax({
+      url: 'https://www.boolean.careers/api/random/mail',
+      method: 'GET',
+      success: function (apiData) {
+
+        var mail = apiData.response;
+        //replace(/[0-9]/g, '') -> Rimuove tutti i numeri da una stringa
+        var name = mail.split('@')[0].replace('.', ' ').replace(/[0-9]/g, '');
+        console.log(name);
+        invitati.push(name);
+
+        if (invitati.length === 10) {
+
+          var tuoNome = prompt('Il tuo nome per entrare.');
+
+          console.log(invitati.includes(tuoNome) ? 'Puoi entrare' : 'Spiacente, sarà per la prossima');
+        }
+      },
+      fail: function (error) {
+        console.log('darrn');
+      }
+    })
+  }
+  */
+
+  /*
+  API SNACK 3
+  Dare la possibilità all’utente di inserire un messaggio attraverso un input,
+  aggiungendo quel messaggio ad un thread (tipo whatsapp).
+  Dopo che il messaggio è stato aggiunto chiedere all’API una frase random
+  (attraverso random/sentence) aggiungendo anch’essa al thread,
+  differenziando i messaggi utente da quelli del computer.
+  */
+  /*
+  var messaggioUtente = prompt('Scrivi un messaggio');
+
+  $.ajax({
+    url: 'https://www.boolean.careers/api/random/sentence',
+    method: 'GET',
+    success: function (apiData) {
+      console.log('You said: ' + messaggioUtente);
+      console.log('API said: ' + apiData.response);
+    },
+    fail: function (error) {
+      console.log(error);
+    }
+  });
+  */
+  /*
+  API Snack 4
+  Chiedere all’utente quanti elementi vuole includere nell’array.
+  Generare attraverso l’API un array contenente N numeri compresi tra 1 e 100.
+  Stampare a schermo la somma dei numeri generati.
+  (www.boolean.careers/api/array/integers, attenzione, serve query string)
+  */
+
+  //var numeri = [];
+  //DA RIVEDERE ENDPOINT
   /*  $.ajax({
       url: 'https://www.boolean.careers/api/array/integers',
       data: {
@@ -29,22 +110,18 @@ $(document).ready(function () {
       },
     });*/
 
-});
+  function generaNumeroCasualeTra(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
-function generaNumeroCasualeTra(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+  /*
+  API Snack 5
+  Creare una griglia 8X8 con jquery.
+  Ogni volta che clicco su un quadrato chiedo all’API un valore boolean random.
+  Se è vero colorerò il quadrato di verde altrimenti di rosso.
+  */
 
-
-/*
-API Snack 5
-Creare una griglia 8X8 con jquery.
-Ogni volta che clicco su un quadrato chiedo all’API un valore boolean random.
-Se è vero colorerò il quadrato di verde altrimenti di rosso.
-*/
-
-$(document).ready(function () {
-
+  /*
   for (var i = 0; i < 64; i++) {
     var cloneQuadratino = $('.quadratino.template').clone();
     cloneQuadratino.removeClass('template');
@@ -76,18 +153,16 @@ $(document).ready(function () {
     }
 
   });
+  */
 
-});
+  /*
+  API Snack 6
+  Creare e stampare a schermo una frase contenente N parole (N in base a
+  quante ne vuole generare l’utente attraverso un input), grazie all’utilizzo
+  dell’API e dell’URL random/word
+  */
 
-/*
-API Snack 6
-Creare e stampare a schermo una frase contenente N parole (N in base a
-quante ne vuole generare l’utente attraverso un input), grazie all’utilizzo
-dell’API e dell’URL random/word
-*/
-
-$(document).ready(function () {
-
+  /*
   var numeroParole = parseInt(prompt('inserisci numero parole'));
   var parole = [];
 
@@ -100,9 +175,11 @@ $(document).ready(function () {
         parole.push(apiData.response);
 
         if (parole.length === numeroParole) {
-          var frase = parole.reduce(function (acc, word, index) {
-            return acc += (word + ' ');
-          }, '');
+          var frase = parole.join(' ');
+          //ALTERNATIVA CON REDUCE
+          // var frase = parole.reduce(function (acc, word, index) {
+          //   return acc += (word + ' ');
+          // }, '');
           console.log(frase);
         }
       },
@@ -112,5 +189,5 @@ $(document).ready(function () {
       },
     });
   }
-
+  */
 });
